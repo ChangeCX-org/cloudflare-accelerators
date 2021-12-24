b4e506ffb6e6f3ce204888aea282af6ad949a894
Dasa Ponnappan <dasa@manizh.com>
2021-12-24 12:05:27 -0500

    Checking in changes to main branch

---
+++ src/txtToJsonConvert.py
@@ -0,0 +1,17 @@
+#Read a file from the current directory named envelopes-rewrite.txt and convert it to JSON
+from json import dumps
+
+file  = open('envelopes-rewrite.txt', 'r')
+data = file.readlines()
+file.close()
+
+with open('rewrites.json', 'w') as file:
+    arr=[]
+    for line in data:
+        key,value = line.split(',')
+        d = {}
+        d["key"] = key
+        d["value"] = value
+        arr.append(d)        
+    file.write(dumps(arr))
+    file.close()
