ec26e84080b054b3ad6f5251069e9fd6079ab454
Dasa Ponnappan <dasa@manizh.com>
2021-12-24 12:20:58 -0500

    Checking in changes for Dockerfile

--- src/txtToJsonConvert.py
+++ src/txtToJsonConvert.py
@@ -1,11 +1,11 @@
 #Read a file from the current directory named envelopes-rewrite.txt and convert it to JSON
 from json import dumps
 
-file  = open('brandname-rewrite.txt', 'r')
+file  = open('rewrites/brandname-rewrite.txt', 'r')
 data = file.readlines()
 file.close()
 
-with open('rewrites.json', 'w') as file:
+with open('rewrites/rewrites.json', 'w') as file:
     arr=[]
     for line in data:
         key,value = line.split(',')
