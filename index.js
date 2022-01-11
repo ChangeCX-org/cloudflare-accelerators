2b2a38493e752f56ef61a22d8163ea0bc04dcc77
Dasa Ponnappan <50528219+dasaep@users.noreply.github.com>
2022-01-11 09:55:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-addEventListener('fetch', event => {
+/**
+ * @author ChangeCX (Dasa Ponnappan)
+ * This worker accesses the name space named REWRITE_MAP mapped to KV store and tries to get a value assocaited to the Key (Which is request.url)
+ */
+
+
+addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  
+  console.log("The request url is " + request)
+  const urlPath = new URL(request.url)
+  const value = await REWRITE_MAP.get(urlPath.pathname)
+  console.log("The URL to be redirected for the provided URL:"+urlPath.pathname+" is :"+value)  
+  urlVal=""
+  if(value) {
+    urlVal = "https://"+urlPath.host+value
+  }else {
+    urlVal = request.url
+  }  
+  return fetch(new Request(urlVal, {
+    body: request.body,
+    headers: request.headers,
+    method: request.method,
+    redirect: request.redirect
   })
+)
 }
